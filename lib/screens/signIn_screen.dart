c18cc8cecbb26005d676f3d37bbdaf59d3db86ae
NemeCharles <106552372+NemeCharles@users.noreply.github.com>
2023-03-26 00:04:49 +0100

    Built the sign up and sign in screens

--- lib/screens/signIn_screen.dart
+++ lib/screens/signIn_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:text_app/screens/signUp_screen.dart';
+import '../components/text_fields.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({Key? key}) : super(key: key);
@@ -8,8 +11,176 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+
+  late final TextEditingController email;
+  late final TextEditingController password;
+
+  @override
+  void initState() {
+    email = TextEditingController();
+    password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    email.dispose();
+    password.dispose();
+    super.dispose();
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: const Color(0XFF0BC4D9),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20.w),
+                height: 140.h,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 30.h,),
+                    Text(
+                      'Sign In',
+                      style: TextStyle(
+                          fontSize: 30.sp,
+                          fontWeight: FontWeight.w500,
+                          color: const Color(0XFFF4F4F4)
+                      ),
+                    ),
+                    SizedBox(height: 10.h,),
+                    Text(
+                      'Login to an account',
+                      style: TextStyle(
+                          fontSize: 17.sp,
+                          fontWeight: FontWeight.w500,
+                          color: const Color(0XFFF4F4F4)
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
+                  width: double.maxFinite,
+                  height: 673.5.h,
+                  decoration: const BoxDecoration(
+                      color: Color(0XFFF4F4F4),
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30)
+                      )
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 25.h,),
+                      Text(
+                        'Enter Email Address',
+                        style: TextStyle(
+                            fontSize: 20.sp,
+                            fontWeight: FontWeight.w500
+                        ),
+                      ),
+                      SizedBox(height: 8.h,),
+                      const TextArea(),
+                      SizedBox(height: 8.h,),
+                      Text(
+                        'Enter Password',
+                        style: TextStyle(
+                            fontSize: 20.sp,
+                            fontWeight: FontWeight.w500
+                        ),
+                      ),
+                      SizedBox(height: 8.h,),
+                      const TextArea(hintText: 'Enter Password', isPassword : true),
+                      SizedBox(height: 10.h,),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          'Forgot password?',
+                          style: TextStyle(
+                            fontSize: 15.sp,
+                            color: Colors.grey.withOpacity(0.7),
+                            wordSpacing: 1.5,
+                            letterSpacing: 0.5,
+                            fontWeight: FontWeight.w500
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20.h,),
+                      Container(
+                        height: 40,
+                        width: 344.w,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                            color: const Color(0XFF0BC4D9),
+                            borderRadius: BorderRadius.circular(15)
+                        ),
+                        child: Text(
+                          'Sign In',
+                          style: TextStyle(
+                              fontSize: 18.sp,
+                              fontWeight: FontWeight.w600,
+                              color: const Color(0XFFF4F4F4)
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 25.h,),
+                      Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          'Or Connect With',
+                          style: TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.w500,
+                            color: Colors.grey.withOpacity(0.7),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 200.h,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Don't have an account? ",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 15,
+                              color: Colors.grey.withOpacity(0.7),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(context,
+                                  MaterialPageRoute(builder: (BuildContext context) => const SignUpScreen()
+                                  )
+                              );
+                            },
+                            child: Text(
+                              'Sign Up',
+                              style: TextStyle(
+                                  color: const Color(0XFF0BC4D9),
+                                  fontSize: 15.sp,
+                                  fontWeight: FontWeight.w500
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  )
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/signUp_screen.dart
+++ lib/screens/signUp_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../components/text_fields.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({Key? key}) : super(key: key);
@@ -8,8 +10,157 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  late final TextEditingController email;
+  late final TextEditingController username;
+  late final TextEditingController password1;
+  late final TextEditingController password2;
+
+  @override
+  void initState() {
+    email = TextEditingController();
+    username = TextEditingController();
+    password1 = TextEditingController();
+    password2 = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    email.dispose();
+    username.dispose();
+    password1.dispose();
+    password2.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: const Color(0XFF0BC4D9),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20.w),
+                height: 140.h,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 30.h,),
+                    Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        fontSize: 30.sp,
+                        fontWeight: FontWeight.w500,
+                        color: const Color(0XFFF4F4F4)
+                      ),
+                    ),
+                    SizedBox(height: 10.h,),
+                    Text(
+                        'Create an account',
+                      style: TextStyle(
+                        fontSize: 17.sp,
+                        fontWeight: FontWeight.w500,
+                        color: const Color(0XFFF4F4F4)
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
+                  width: double.maxFinite,
+                  height: 673.5.h,
+                  decoration: const BoxDecoration(
+                  color: Color(0XFFF4F4F4),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30)
+                  )
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 25.h,),
+                    Text(
+                        'Enter Username',
+                      style: TextStyle(
+                        fontSize: 20.sp,
+                        fontWeight: FontWeight.w500
+                      ),
+                    ),
+                    SizedBox(height: 8.h,),
+                    const TextArea(hintText: 'Enter Username', isUsername: true),
+                    SizedBox(height: 8.h,),
+                    Text(
+                        'Enter Email Address',
+                      style: TextStyle(
+                        fontSize: 20.sp,
+                        fontWeight: FontWeight.w500
+                      ),
+                    ),
+                    SizedBox(height: 8.h,),
+                    const TextArea(),
+                    SizedBox(height: 8.h,),
+                    Text(
+                        'Create Password',
+                      style: TextStyle(
+                          fontSize: 20.sp,
+                          fontWeight: FontWeight.w500
+                      ),
+                    ),
+                    SizedBox(height: 8.h,),
+                    const TextArea(hintText: 'Enter Password', isPassword : true),
+                    SizedBox(height: 8.h,),
+                    Text(
+                      'Re-Enter Password',
+                      style: TextStyle(
+                          fontSize: 20.sp,
+                          fontWeight: FontWeight.w500
+                      ),
+                    ),
+                    SizedBox(height: 8.h,),
+                    const TextArea(hintText: 'Re-Enter Password', isPassword : true),
+                    SizedBox(height: 25.h,),
+                    Container(
+                      height: 40,
+                      width: 344.w,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          color: const Color(0XFF0BC4D9),
+                          borderRadius: BorderRadius.circular(15)
+                      ),
+                      child: Text(
+                        'Sign In',
+                        style: TextStyle(
+                            fontSize: 18.sp,
+                            fontWeight: FontWeight.w600,
+                            color: const Color(0XFFF4F4F4)
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 25.h,),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                          'Or Connect With',
+                        style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.w500,
+                          color: Colors.grey.withOpacity(0.7),
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
+
