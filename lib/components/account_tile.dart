4a45c92f3a39547904b0dc96c5966f0990dc1eb7
NemeCharles <106552372+NemeCharles@users.noreply.github.com>
2023-03-29 19:24:42 +0200

    Created model

---
+++ lib/components/account_tile.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AccountImageTile extends StatelessWidget {
+  const AccountImageTile({
+    super.key, required this.imagePath,
+  });
+
+  final String imagePath;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 8.0.w),
+      child: Container(
+        height: 50.h,
+        width: 50.w,
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+          color: Colors.grey.withOpacity(0.08),
+        ),
+        child: Image.asset('images/$imagePath', ),
+      ),
+    );
+  }
+}
