34710da2dab007f2840e356189cf61f3aa205c2f
NemeCharles <106552372+NemeCharles@users.noreply.github.com>
2023-03-30 21:30:14 +0200

    Added home_screen file

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,6 +10,32 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              width: double.maxFinite,
+              height: 250,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      const Text('Talkrr'),
+                      SizedBox(
+                        child: Row(
+                          children: [],
+                        ),
+                      )
+                    ],
+                  ),
+
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
