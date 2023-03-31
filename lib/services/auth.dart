c8b2a49c25666037ca5a9ec39ce8f974d97be43b
NemeCharles <106552372+NemeCharles@users.noreply.github.com>
2023-03-31 13:33:29 +0200

    Implemented cloud database for saving and getting user info

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,16 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:text_app/user_model/user.dart';
 
 class FireAuth {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final CollectionReference _userStore = FirebaseFirestore.instance.collection('user');
 
   // Sign Up with Email and Password
 
-  Future<void> signUpEmail (String email, String password) async {
+  Future<void> signUpEmail (String email, String password, String displayName) async {
     try{
       UserCredential user = await _auth.
       createUserWithEmailAndPassword(email: email, password: password);
       print('SUCCESSFUL');
-      print(user);
+
+      if(user.user != null) {
+        await _userStore.withConverter<UserData>(
+            fromFirestore: (snapshot, _) => UserData.fromJson(snapshot.data()!),
+            toFirestore: (userData, _) => userData.toJson()
+        ).add(
+            UserData(
+              displayName: displayName,
+              email: email,
+              uid: user.user?.uid,
+              profilePhoto: '',
+            )
+        ).then((value) => print('USER SAVED SUCCESSFULLY'));
+      }
     } on FirebaseAuthException catch (e) {
       print(e.code);
     } catch (e) {
@@ -24,7 +40,14 @@ class FireAuth {
     try{
       UserCredential user = await _auth.
       signInWithEmailAndPassword(email: email, password: password);
-      print(user);
+      print('SIGN IN SUCCESSFUL');
+
+      await _userStore.withConverter<UserData>(
+          fromFirestore: (snapshot, _) => UserData.fromJson(snapshot.data()!),
+          toFirestore: (userData, _) => userData.toJson()
+      ).where('uid', isEqualTo: user.user?.uid).get().then((value) =>
+          print('USERNAME: ${value.docs.first.data().displayName}'));
+
     } on FirebaseAuthException catch (e) {
       print(e.code);
     } catch (e) {
