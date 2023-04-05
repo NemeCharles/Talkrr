f308fe6ef0a945fe9f2792c52fe5da7d1f630d2f
NemeCharles <106552372+NemeCharles@users.noreply.github.com>
2023-04-05 18:06:46 +0200

    Created Hive services class

---
+++ lib/storage_services/hive_services.dart
@@ -0,0 +1,39 @@
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:text_app/storage_services/user_info/user_info.dart';
+
+const String _userDataStorageKey = 'USER_DATA_STORAGE_KEY';
+const String _boxName = 'USER_DATA';
+
+class HiveServices {
+
+  final _box = Hive.box<LoggedInUserData>(_boxName);
+
+  static Future<void> initialiseBoxes() async {
+    try{
+      await Hive.initFlutter();
+      Hive.registerAdapter(LoggedInUserDataAdapter());
+      await Hive.openBox<LoggedInUserData>(_boxName);
+    } catch(e) {
+      print(e);
+    }
+
+  }
+
+  Future<void> addUser(LoggedInUserData value) async {
+    await _box.put(_userDataStorageKey, value);
+  }
+
+  Future<LoggedInUserData?> getUser() async {
+    final user = _box.get(_userDataStorageKey);
+    return user;
+  }
+
+  Future<void> deleteUser() async {
+    await _box.delete(_userDataStorageKey);
+  }
+
+  Future<void> closeBox() async {
+    await _box.close();
+  }
+
+}
