21872cbbf46cf0156a87a8730feffe84260cf83d
NemeCharles <106552372+NemeCharles@users.noreply.github.com>
2023-04-30 17:06:46 +0200

    made changes to the media screen

--- lib/screens/media_screen/media_screen.dart
+++ lib/screens/media_screen/media_screen.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:photo_manager/photo_manager.dart';
+import 'package:text_app/screens/media_screen/media_view.dart';
 
 
 class MediaScreen extends StatefulWidget {
@@ -14,14 +17,14 @@ class MediaScreen extends StatefulWidget {
 class _MediaScreenState extends State<MediaScreen> {
 
   int currentPage = 0;
-  late Future<List<Uint8List?>> photos;
+  late Future<List<File?>> photos;
 
   void loadPhotos() {
     photos = requestAssets();
   }
 
-  Future<List<Uint8List?>> requestAssets() async {
-    List<Uint8List?> thumbs = [];
+  Future<List<File?>> requestAssets() async {
+    List<File?> test = [];
     final PermissionState permission = await PhotoManager.requestPermissionExtend();
     if(!permission.isAuth) {
       await PhotoManager.openSetting();
@@ -36,11 +39,12 @@ class _MediaScreenState extends State<MediaScreen> {
           size: 30
       );
       for(AssetEntity image in pathImages) {
-        final thumb = await image.thumbnailDataWithSize(const ThumbnailSize(200, 200));
-        thumbs.add(thumb);
+        // final thumb = await image.thumbnailDataWithSize(const ThumbnailSize(200, 200));
+        final thumb = await image.file;
+        test.add(thumb);
       }
     }
-    return thumbs;
+    return test;
   }
 
   @override
@@ -90,11 +94,16 @@ class _MediaScreenState extends State<MediaScreen> {
                 itemCount: snapshot.data!.length,
                 itemBuilder: (_, index) {
                   return InkWell(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (BuildContext context) =>  MediaViewScreen(image: snapshot.data![index]!,))
+                      );
+                    },
                     child: Container(
                       margin: const EdgeInsets.all(1),
                       decoration: BoxDecoration(
                           image: DecorationImage(
-                              image: MemoryImage(snapshot.data![index]!),
+                              image: FileImage(snapshot.data![index]!),
                               fit: BoxFit.cover
                           )
                       ),
