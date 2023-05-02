4713d0d62d87632784dc7d286d71debf5ca8a9fa
NemeCharles <106552372+NemeCharles@users.noreply.github.com>
2023-05-02 14:52:55 +0200

    Re-wrote media_view.dart file

--- lib/screens/media_screen/media_view.dart
+++ lib/screens/media_screen/media_view.dart
@@ -3,10 +3,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../components/text_fields.dart';
 
-class MediaViewScreen extends StatelessWidget {
+class MediaViewScreen extends StatefulWidget {
   const MediaViewScreen({Key? key, required this.image}) : super(key: key);
 
   final File image;
+
+  @override
+  State<MediaViewScreen> createState() => _MediaViewScreenState();
+}
+
+class _MediaViewScreenState extends State<MediaViewScreen> {
 
   @override
   Widget build(BuildContext context) {
@@ -18,23 +24,52 @@ class MediaViewScreen extends StatelessWidget {
           children: [
             SingleChildScrollView(
               scrollDirection: Axis.vertical,
-              child: Column(
-                children: [
-                  SizedBox(height: 30.h,),
-                  ConstrainedBox(
-                    constraints: BoxConstraints(
-                      maxHeight: 740.h
-                    ),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: FileImage(image)
-                        )
-                      ),
+              child: Container(
+                margin: const EdgeInsets.only(top: 55),
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(
+                    maxHeight: 700.h
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: FileImage(widget.image)
+                      )
                     ),
                   ),
-                ],
-          ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 70.h,
+              left: 10.w,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: InkWell(
+                  splashColor: Colors.transparent,
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  borderRadius: BorderRadius.circular(50),
+                  child: SizedBox(
+                    height: 45.h,
+                    width: 45.w,
+                    child:  Center(
+                      child: Icon(
+                      Icons.close,
+                      size: 33,
+                      color:  Colors.white,
+                      shadows: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.6),
+                          spreadRadius: 5,
+                          blurRadius: 5,
+                        )
+                      ],
+                    ),),
+                  ),
+                ),
+              ),
             ),
             Positioned(
               bottom: 0,
@@ -46,18 +81,24 @@ class MediaViewScreen extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                       const MediaMessageField(),
-                      InkWell(
-                        child: Container(
-                          height: 45.h,
-                          width: 45.w,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(50),
-                              color: const Color(0XFF202020)
-                          ),
-                          child: const Center(
-                            child: Icon(
-                              Icons.send,
-                              color: Color(0XFF0BC4D9),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(50),
+                        child: InkWell(
+                          splashColor: Colors.transparent,
+                          onTap: () async {},
+                          borderRadius: BorderRadius.circular(50),
+                          child: Container(
+                            height: 45.h,
+                            width: 45.w,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                color: const Color(0XFF202020)
+                            ),
+                            child: const Center(
+                              child: Icon(
+                                Icons.send,
+                                color: Color(0XFF0BC4D9),
+                              ),
                             ),
                           ),
                         ),
@@ -73,4 +114,3 @@ class MediaViewScreen extends StatelessWidget {
     );
   }
 }
-
