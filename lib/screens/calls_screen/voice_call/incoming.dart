f4236c39646ff0ad55fecf42371412e122be9a6b
NemeCharles <106552372+NemeCharles@users.noreply.github.com>
2023-05-15 00:45:06 +0200

    Made changes to file

--- lib/screens/calls_screen/voice_call/incoming.dart
+++ lib/screens/calls_screen/voice_call/incoming.dart
@@ -1,6 +1,8 @@
 import 'dart:ui';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../models/calls.dart';
 import '../../../services/calls.dart';
 import 'outgoing.dart';
 
@@ -21,132 +23,144 @@ class _IncomingCallScreenState extends State<IncomingCallScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0XFFF4F4F4),
-      body: Stack(
-        children: [
-          Container(
-            height: 838.3.h,
-            width: 384.w,
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage(testImage,),
-                  fit: BoxFit.cover,
-                )
-            ),
-          ),
-          ClipRect(
-            child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-              child: Stack(
-                children: [
-                  Positioned(
-                    top: 160.h,
-                    child: SizedBox(
-                      height: 200.h,
-                      width: 384.w,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            height: 120.h,
-                            width: 120.w,
-                            decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                image: DecorationImage(
-                                  image: NetworkImage(testImage,),
-                                  fit: BoxFit.cover,
+      body: StreamBuilder<DocumentSnapshot<CallModel?>>(
+        stream: _manager.watchForCalls(),
+        builder: (context, snapshot) {
+          final caller = snapshot.data?.data();
+          if(caller != null) {
+            return Stack(
+              children: [
+                Container(
+                  height: 838.3.h,
+                  width: 384.w,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(testImage,),
+                        fit: BoxFit.cover,
+                      )
+                  ),
+                ),
+                ClipRect(
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          top: 160.h,
+                          child: SizedBox(
+                            height: 200.h,
+                            width: 384.w,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  height: 120.h,
+                                  width: 120.w,
+                                  decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      image: DecorationImage(
+                                        image: NetworkImage(testImage,),
+                                        fit: BoxFit.cover,
+                                      )
+                                  ),
+                                ),
+                                SizedBox(height: 12.h,),
+                                Text(
+                                  caller.callerName ?? '',
+                                  style: TextStyle(
+                                      fontSize: 30.sp,
+                                      fontWeight: FontWeight.w400,
+                                      color: const Color(0XFFF4F4F4),
+                                      shadows: const [
+                                        BoxShadow(
+                                            blurRadius: 1,
+                                            spreadRadius: 3
+                                        )
+                                      ]
+                                  ),
+                                ),
+                                SizedBox(height: 6.h,),
+                                Text(
+                                  caller.isVoiceCall
+                                      ? 'Incoming voice call...'
+                                      : 'Incoming video call...',
+                                  style: TextStyle(
+                                      color: const Color(0XFFF4F4F4),
+                                      fontSize: 13.sp,
+                                      fontWeight: FontWeight.w400,
+                                      letterSpacing: 1,
+                                      shadows: const [
+                                        BoxShadow(
+                                            blurRadius: 1,
+                                            spreadRadius: 2
+                                        )
+                                      ]
+                                  ),
                                 )
+                              ],
                             ),
                           ),
-                          SizedBox(height: 12.h,),
-                          Text(
-                            'Grim Reaper',
-                            style: TextStyle(
-                              fontSize: 30.sp,
-                              fontWeight: FontWeight.w400,
-                              color: const Color(0XFFF4F4F4),
-                              shadows: const [
-                                BoxShadow(
-                                  blurRadius: 1,
-                                  spreadRadius: 3
-                                )
-                              ]
+                        ),
+                        Positioned(
+                          bottom: 110.h,
+                          left: 60.w,
+                          child: GestureDetector(
+                            onTap: () {
+                              _manager.endCall();
+                            },
+                            child: Container(
+                              height: 65.h,
+                              width: 65.w,
+                              alignment: Alignment.center,
+                              decoration: const BoxDecoration(
+                                  color: Colors.red,
+                                  shape: BoxShape.circle
+                              ),
+                              child: Icon(
+                                Icons.call_end_outlined,
+                                color: Colors.white,
+                                size: 35.sp,
+                              ),
                             ),
                           ),
-                          SizedBox(height: 6.h,),
-                          Text(
-                            'Incoming call...',
-                            style: TextStyle(
-                              color: const Color(0XFFF4F4F4),
-                              fontSize: 13.sp,
-                              fontWeight: FontWeight.w400,
-                              letterSpacing: 1,
-                              shadows: const [
-                                BoxShadow(
-                                    blurRadius: 1,
-                                    spreadRadius: 2
-                                )
-                              ]
+                        ),
+                        Positioned(
+                          bottom: 110.h,
+                          right: 60.w,
+                          child: GestureDetector(
+                            onTap: () async {
+                              Navigator.push(context,
+                                  MaterialPageRoute(builder: (
+                                      BuildContext context) => const OutgoingCall())
+                              );
+                              await Future.delayed(const Duration(seconds: 1),);
+                              widget.entry.remove();
+                            },
+                            child: Container(
+                              height: 65.h,
+                              width: 65.w,
+                              alignment: Alignment.center,
+                              decoration: const BoxDecoration(
+                                  color: Colors.green,
+                                  shape: BoxShape.circle
+                              ),
+                              child: Icon(
+                                Icons.call,
+                                color: Colors.white,
+                                size: 35.sp,
+                              ),
                             ),
-                          )
-                        ],
-                      ),
+                          ),
+                        )
+                      ],
                     ),
                   ),
-                  Positioned(
-                    bottom: 110.h,
-                    left: 60.w,
-                    child: GestureDetector(
-                      onTap: () {
-                        _manager.createInstance();
-                      },
-                      child: Container(
-                        height: 65.h,
-                        width: 65.w,
-                        alignment: Alignment.center,
-                        decoration: const BoxDecoration(
-                            color: Colors.red,
-                            shape: BoxShape.circle
-                        ),
-                        child: Icon(
-                          Icons.call_end_outlined,
-                          color: Colors.white,
-                          size: 35.sp,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    bottom: 110.h,
-                    right: 60.w,
-                    child: GestureDetector(
-                      onTap: () async {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (BuildContext context) => const OutgoingCall())
-                        );
-                        await Future.delayed(const Duration(seconds: 1),);
-                        widget.entry.remove();
-                      },
-                      child: Container(
-                        height: 65.h,
-                        width: 65.w,
-                        alignment: Alignment.center,
-                        decoration: const BoxDecoration(
-                            color: Colors.green,
-                            shape: BoxShape.circle
-                        ),
-                        child: Icon(
-                          Icons.call,
-                          color: Colors.white,
-                          size: 35.sp,
-                        ),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-        ],
+                ),
+              ],
+            );
+          }
+          return const SizedBox();
+        }
       ),
     );
   }
