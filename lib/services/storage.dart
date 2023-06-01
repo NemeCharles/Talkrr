0daea635d9f3e75f236e25990a788d53966a6658
NemeCharles <106552372+NemeCharles@users.noreply.github.com>
2023-06-01 04:05:38 +0200

    Converted to Riverpod provider

--- lib/services/storage.dart
+++ lib/services/storage.dart
@@ -1,14 +1,17 @@
 import 'package:firebase_storage/firebase_storage.dart';
 import 'dart:io';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final storagePod = Provider<Storage>((ref) => Storage());
 
 class Storage {
 
   final store = FirebaseStorage.instance.ref();
 
-  Future<String> storeImage({required File file, required String reference}) async {
+  Future<String> storeFileToFirebase({required File file, required String reference}) async {
     String url = '';
     try{
-      final imageRef = store.child('senderID/userID/images/gil');
+      final imageRef = store.child(reference);
       final UploadTask task = imageRef.putFile(file);
       await task.whenComplete((){});
       String downloadUrl = await imageRef.getDownloadURL();
@@ -22,20 +25,5 @@ class Storage {
 
   }
 
-  Future<String> uploadImage(File file) async {
-    String url = '';
-    try {
-      final imageRef = store.child('senderID/userID/images/gil');
-      final UploadTask task = imageRef.putFile(file);
-      await task.whenComplete((){});
-      String downloadUrl = await imageRef.getDownloadURL();
-      url = downloadUrl;
-    } on FirebaseException catch (e) {
-      print(e.toString());
-    } catch (e) {
-      print(e);
-    }
 
-    return url;
-  }
 }
